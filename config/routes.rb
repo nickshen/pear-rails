b80e0b9497c0cb6813486af14e8e7b4a6e3b70f0
pkmnfreak <evcchang@berkeley.edu>
2016-11-10 16:00:08 -0800

    Added Name Attribute

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => :registrations }
   resources :users
   
   # The priority is based upon order of creation: first created -> highest priority.
