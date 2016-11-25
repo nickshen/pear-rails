a6fad9039f81e7971cd53c6195e8a3dccf1dfa3d
Nick Shen <nickzshen@gmail.com>
2016-11-24 18:03:43 -0800

    self user profile column on main view, edited user_edit_profile slightly, styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :users, :controllers => { :registrations => :registrations }
+
   resources :users
   resources :conversations do
     resources :messages
