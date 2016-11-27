1fe90e43ba3540cf35a4fb74e10db7a4fde01b77
Nick Shen <nickzshen@gmail.com>
2016-11-27 04:54:50 -0800

    refactor conversation permissions to controller side instead of client, distinction for no convos

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,9 +1,10 @@
 class ConversationsController < ApplicationController
 	before_action :authenticate_user!
-
+	
 	def index
 		@users = User.all
-		@conversations = Conversation.all
+		conversations = Conversation.all
+		@conversations = conversations.where('sender_id= ? OR recipient_id= ?', current_user.id, current_user.id)
 	end
 
 	def create
