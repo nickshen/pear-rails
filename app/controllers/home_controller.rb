5f725941e872297042dd333eb1a8f8e108e0fb36
pkmnfreak <evcchang@berkeley.edu>
2016-11-29 18:22:53 -0800

    User requests now say who you requested

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,10 +8,10 @@ class HomeController < ApplicationController
 
   def index
     if current_user
-      @User = User.where.not(id: current_user.id).where.not(id: current_user.requests).order('RANDOM()').first
+      if User.where.not(id: current_user.id).where.not(id: current_user.pending_matches).where.not(id: current_user.requests).any?
+        @User = User.where.not(id: current_user.id).where.not(id: current_user.pending_matches).where.not(id: current_user.requests).order('RANDOM()').first
+      end
     end
   end
 
-  def _match
-  end
 end
