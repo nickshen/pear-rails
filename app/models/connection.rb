fb9899eaa4ecbcef61b1a75fcad1ffe37aaeb339
Nick Shen <nickzshen@gmail.com>
2016-11-29 19:29:52 -0800

    matching done?:

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,14 +1,18 @@
 class Connection < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :match, class_name: 'User'
-
   after_create :create_inverse_relationship
   after_destroy :destroy_inverse_relationship
+
+  belongs_to :user
+  belongs_to :match, class_name: 'User'
 
   private
 
   def create_inverse_relationship
-    match.connection.create(match: user)
+    if Connection.find_by(match_id: user.id).nil?
+      match.connections.create(match: user)
+    else
+      true
+    end
   end
 
   def destroy_inverse_relationship
