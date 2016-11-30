920db5d220fef6bdaa487a44386b80dba3caaf6d
pkmnfreak <evcchang@berkeley.edu>
2016-11-29 20:46:12 -0800

    Random Users but connections

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,8 +8,14 @@ class HomeController < ApplicationController
 
   def index
     if current_user
-      if User.where.not(id: current_user.id).where.not(id: current_user.pending_matches).where.not(id: current_user.requests).any?
-        @User = User.where.not(id: current_user.id).where.not(id: current_user.pending_matches).where.not(id: current_user.requests).order('RANDOM()').first
+      if current_user.connections.any?
+        if User.where.not(id: current_user.id).where.not(id: current_user.requests.select("match_id")).where.not(current_user.connections.find_by(match_id: @user_on_home.id)).any?
+          @User = User.where.not(id: current_user.id).where.not(id: current_user.requests.select("match_id")).where.not(current_user.connections.find_by(match_id: @user_on_home.id)).order('RANDOM()').first
+        end
+      else
+        if User.where.not(id: current_user.id).where.not(id: current_user.requests.select("match_id")).any?
+          @User = User.where.not(id: current_user.id).where.not(id: current_user.requests.select("match_id")).order('RANDOM()').first
+        end
       end
     end
   end
